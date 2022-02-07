e9d744182bca4b1750d8ea70dc91e6b0894030d4
eapenzacharias <eapenzac@gmail.com>
2022-02-08 00:06:06 +0100

    added parent_permission to super

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown')
-    super(age, name)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
